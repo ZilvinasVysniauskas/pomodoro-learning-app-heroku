3ad42b86674488a3aacab61a7ba5ee16a6ff7a0a
ZilvinasVysniauskas <zalavakas@gmail.com>
2022-01-17 10:35:11 +0200

    test

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
+
 <?php
-echo 'this is test';
+try {
+    include __DIR__ . '/../includes/autoload.php';
+
+    $route = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
+
+    $entryPoint = new \Ninja\EntryPoint($route, $_SERVER['REQUEST_METHOD'], new \Pomodoro\PomodoroRoutes());
+    $entryPoint->run();
+}
+catch (PDOException $e) {
+    $title = 'An error has occurred';
+
+    $output = 'Database error: ' . $e->getMessage() . ' in ' .
+        $e->getFile() . ':' . $e->getLine();
+    include  __DIR__ . '/../templates/layout.html.php';
+}
