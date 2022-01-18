47e91c341ce82b846c8e72da1cd9aa972db6ff35
ZilvinasVysniauskas <zalavakas@gmail.com>
2022-01-18 11:14:28 +0200

    x

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,6 +1,6 @@
 <?php
 
-$pdo = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_f51ee3974c0c563;charset=utf8', 'ba4ab38e16a343', 'ad4507d8');
+$pdo = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_b3d810be4013062;charset=utf8', 'ba4ab38e16a343', 'ad4507d8');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
