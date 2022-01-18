c320e2dddaf317f37d267541e56085864e12e01d
ZilvinasVysniauskas <zalavakas@gmail.com>
2022-01-18 23:16:01 +0200

    x

--- javascript/timer.js.php
+++ javascript/timer.js.php
@@ -292,7 +292,7 @@ $(document).ready(function (){
         $.post("/ajax/updateAjax.php", {
             taskToAdd: taskToAdd
         });
-        setTimeout(function(){$('#tasksFromDb').load('/ajax/loadTasksFromDbAjax.php')}, 5000);
+        setTimeout(function(){$('#tasksFromDb').load('/ajax/loadTasksFromDbAjax.php')}, 1);
 
         document.getElementById('addTaskForm').style.display = 'none';
         document.getElementById('userTaskInput').value = '';
