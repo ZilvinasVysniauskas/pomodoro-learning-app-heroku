035576ae8446537503685a4898e33f164c1e4078
ZilvinasVysniauskas <zalavakas@gmail.com>
2022-01-18 09:02:56 +0200

    tst

--- public/index.php
+++ public/index.php
@@ -3,16 +3,6 @@ try {
     include __DIR__ . '/../includes/autoload.php';
 
     $route = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
-    echo '<script>console.log(' . $route .')
-var delayInMilliseconds = 10000; //1 second
-
-setTimeout(function() {
-  //your code to be executed after 1 second
-}, delayInMilliseconds);
-
-</script>';
-
-
     $entryPoint = new \Ninja\EntryPoint($route, $_SERVER['REQUEST_METHOD'], new \Pomodoro\PomodoroRoutes());
     $entryPoint->run();
 }
